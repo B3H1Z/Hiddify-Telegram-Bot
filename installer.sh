875b681340167dc4c02b7d77f53b51218f603484
Behnam <behnamahmadi1@protonmail.com>
2023-07-23 01:13:15 +0330

    some changes

--- installer.sh
+++ installer.sh
@@ -5,7 +5,7 @@ APP_NAME="Hiddify-Telegram-Bot"
 APP_VERSION="0.1.0"
 
 # Set the GitHub repository URL
-GITHUB_REPO="https://github.com/yourusername/your-repo.git"
+GITHUB_REPO="https://github.com/B3H1Z/Hiddify-Telegram-Bot.git"
 
 # Define the installation directory
 INSTALL_DIR="/usr/local/$APP_NAME"
@@ -27,12 +27,12 @@ TMP_DIR=$(mktemp -d)
 git clone "$GITHUB_REPO" "$TMP_DIR" --branch "$APP_VERSION" --depth 1
 
 # Install the Python app
-pip3 install "$TMP_DIR"
+pip3 install -r "$TMP_DIR/requirements.txt" --target "$INSTALL_DIR"
 
 # Clean up temporary directory
 rm -rf "$TMP_DIR"
 
 # Create a symlink to the installed app in /usr/local/bin
-ln -s "$INSTALL_DIR/bin/your-python-app" "/usr/local/bin/your-python-app"
+ln -s "$INSTALL_DIR/bin/hidi-bot" "/usr/local/bin/hidi-bot"
 
-echo "Installation complete. You can now use the 'your-python-app' command."
+echo "Installation complete."
