a2ffebaa7121e75e7594a056223df7c8e4991682
Behnam <behnamahmadi1@protonmail.com>
2023-08-06 04:52:42 +0330

    Main File

---
+++ hidyBot.py
@@ -0,0 +1,9 @@
+import AdminBot.bot
+from threading import Thread
+from config import CLIENT_TOKEN
+
+if __name__ == '__main__':
+    Thread(target=AdminBot.bot.start).start()
+    if CLIENT_TOKEN:
+        import UserBot.bot
+        Thread(target=UserBot.bot.start).start()
