1f64758a0ae118a71425625ba18eeba9c3ec0fa9
Behnam <behnamahmadi1@protonmail.com>
2023-09-12 14:58:42 +0330

    fix bugs

--- install.sh
+++ install.sh
@@ -21,6 +21,13 @@ if ! command -v python3 &>/dev/null || ! command -v pip &>/dev/null; then
   display_error_and_exit "Python 3 and pip are required. Please install them and try again."
 fi
 
+if [ "$1" == "pre-release" ]; then
+  branch="pre-release"
+  echo -e "${RED}Installing pre-release version...${RED}"
+else
+  branch="main" # Default branch
+fi
+
 echo -e "${GREEN}Step 1: Cloning the repository and changing directory...${RESET}"
 repository_url="https://github.com/B3H1Z/Hiddify-Telegram-Bot.git"
 install_dir="/opt/Hiddify-Telegram-Bot"
@@ -28,13 +35,29 @@ install_dir="/opt/Hiddify-Telegram-Bot"
 if [ -d "$install_dir" ]; then
   echo "Directory $install_dir exists."
 else
-  git clone "$repository_url" "$install_dir" || display_error_and_exit "Failed to clone the repository."
+  git clone -b "$branch" "$repository_url" "$install_dir" || display_error_and_exit "Failed to clone the repository."
+  echo -e "${GREEN}${branch}${RESET}"
 fi
 
 cd "$install_dir" || display_error_and_exit "Failed to change directory."
 
 echo -e "${GREEN}Step 2: Installing requirements...${RESET}"
-pip install -r requirements.txt || display_error_and_exit "Failed to install requirements."
+if ! pip install -q -r requirements.txt 2>&1 | grep -q "AttributeError: module 'lib' has no attribute 'X509_V_FLAG_CB_ISSUER_CHECK'"; then
+  echo -e "${RED}Failed to install requirements. Removing existing pyopenssl and reinstalling...${RESET}"
+
+  # Remove existing pyopenssl using your original command
+  if ! sudo rm -rf /usr/lib/python3/dist-packages/OpenSSL; then
+    display_error_and_exit "Failed to remove existing pyopenssl. Please check for errors and try again."
+  fi
+
+  # Install pyopenssl
+  if ! sudo pip3 install pyopenssl; then
+    display_error_and_exit "Failed to install pyopenssl. Please check for errors and try again."
+  else
+    echo -e "${GREEN}pyopenssl has been installed.${RESET}"
+  fi
+fi
+
 
 echo -e "${GREEN}Step 3: Preparing ...${RESET}"
 logs_dir="$install_dir/Logs"
@@ -61,26 +84,21 @@ nohup python3 hiddifyTelegramBot.py >>/opt/Hiddify-Telegram-Bot/bot.log 2>&1 &
 
 echo -e "${GREEN}Step 6: Adding cron jobs...${RESET}"
 
-if ! crontab -l | grep "@reboot cd $install_dir && ./restart.sh"; then
-  (
-    crontab -l 2>/dev/null
-    echo "@reboot cd $install_dir && ./restart.sh"
-  ) | crontab -
-fi
+add_cron_job_if_not_exists() {
+  local cron_job="$1"
+  if ! crontab -l | grep -q "$cron_job"; then
+    (crontab -l 2>/dev/null; echo "$cron_job") | crontab -
+  fi
+}
 
-if ! crontab -l | grep "0 */6 * * * cd $install_dir && python3 crontab.py --backup"; then
-  (
-    crontab -l 2>/dev/null
-    echo "0 */6 * * * cd $install_dir && python3 crontab.py --backup"
-  ) | crontab -
-fi
+# Add cron job for reboot
+add_cron_job_if_not_exists "@reboot cd $install_dir && ./restart.sh"
 
-if ! crontab -l | grep "0 12 * * * cd $install_dir && python3 crontab.py --reminder"; then
-  (
-    crontab -l 2>/dev/null
-    echo "0 12 * * * cd $install_dir && python3 crontab.py --reminder"
-  ) | crontab -
-fi
+# Add cron job to run every 6 hours
+add_cron_job_if_not_exists "0 */6 * * * cd $install_dir && python3 crontab.py --backup"
+
+# Add cron job to run at 12:00 PM daily
+add_cron_job_if_not_exists "0 12 * * * cd $install_dir && python3 crontab.py --reminder"
 
 echo -e "${GREEN}Waiting for a few seconds...${RESET}"
 sleep 5
