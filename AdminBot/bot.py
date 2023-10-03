b78cd51987c388d0921754456e1bb52397a0e4c4
Behnam <behnamahmadi1@protonmail.com>
2023-10-03 05:51:03 +0330

    fix delete server

--- AdminBot/bot.py
+++ AdminBot/bot.py
@@ -1498,6 +1498,8 @@ def callback_query(call: CallbackQuery):
         servers = USERS_DB.select_servers()
         bot.edit_message_text(KEY_MARKUP['SERVERS_MANAGEMENT'], call.message.chat.id, call.message.message_id,
                                     reply_markup=markups.servers_management_markup(servers)) 
+        bot.send_message(call.message.chat.id, MESSAGES['SUCCESS_REMOVED_SERVER'], reply_markup=makups.main_menu_keyboard_markup())
+
         
     # Server Management - List of Plans for Server Callback
     elif key == "server_list_of_plans":
@@ -1511,7 +1513,6 @@ def callback_query(call: CallbackQuery):
         plans_markup = markups.plans_list_markup(plans_list,value)
         bot.edit_message_text({MESSAGES['USERS_BOT_PLANS_LIST']}, call.message.chat.id, call.message.message_id,
                          reply_markup=plans_markup)
-        bot.send_message(call.message.chat.id, MESSAGES['SUCCESS_REMOVED_SERVER'], reply_markup=makups.main_menu_keyboard_markup())
         
     elif key == "server_list_of_users":
         users_list = api.select(URL)
